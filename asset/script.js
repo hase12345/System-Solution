93b10231f5ddacb39b2567c4fb65b805c8425d8d
hase12345 <49516827+hase12345@users.noreply.github.com>
2020-09-12 21:46:05 +0900

    Add files via upload

---
+++ asset/script.js
@@ -0,0 +1,24 @@
+$(() => {
+  $(window).scroll(() => {
+    if ($(this).scrollTop() > 10) {
+      $('header').css({
+        'background-color':'#000',
+        'border':'0'
+      })
+    } else {
+      $('header').css({
+        'background-color':'transparent',
+        'border-bottom':'1px solid #fff'
+      })
+    }
+  });
+
+  $('.drawer').drawer();
+
+  $('.header__logo, .footer__title').click(() =>{
+    $('html').animate({
+      scrollTop:0
+    },800);
+    return false;
+  })
+});
